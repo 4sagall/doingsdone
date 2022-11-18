53c68847b5f337c489f69a3babe9d78d6334aee7
sagall <4sagall@gmail.com>
2022-11-18 16:15:38 +0300

    modified init.php

--- init.php
+++ init.php
@@ -2,8 +2,8 @@
 
 require_once('functions.php');
 
-$link = mysqli_connect('127.0.0.1', 'root', 'root', 'doingsdone');
-mysqli_set_charset($link, charset:"utf8");
+$link = mysqli_connect("127.0.0.1", "root", "", "doingsdone");
+mysqli_set_charset($link, charset: "utf8");
 
 $projects = [];
 $tasks = [];
